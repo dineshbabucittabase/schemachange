583d6f37a41cb43366bea2a215ae376744c3dbf1
dineshbabucittabas <dinesh.b@cittabase.com>
2023-12-14 18:41:59 +0530

    deleting the files

--- migrations/R__first_repeatable_script.sql
+++
@@ -1,6 +0,0 @@
-CREATE or replace VIEW myview COMMENT='Test view' AS 
-SELECT 
-    1 as col1,
-    2 as col2,
-    3 as col3,
-    4 as col4;         
--- migrations/V1.1.4__insert.sql
+++
@@ -1,1 +0,0 @@
-insert into release_entry select test_seq.nextval as seq , current_timestamp() as Release_time;
