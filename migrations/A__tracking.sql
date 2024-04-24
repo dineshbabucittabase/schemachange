d88ac008ab369d040d8b3b087ef788f2e6fa18e0
dineshbabucittabas <dinesh.b@cittabase.com>
2024-04-24 22:48:00 +0530

    corrected

--- migrations/A__tracking.sql
+++ migrations/A__tracking.sql
@@ -1,4 +1,4 @@
 create table if not exists release_entry as select seq4() as seq , current_timestamp() as Release_time;
 
 insert into release_entry
- select test_seq.nextval as seq , current_timestamp() as Release_time;
+ select seq4() as seq , current_timestamp() as Release_time;
