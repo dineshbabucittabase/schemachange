36589a7295ded350e066a72d1ec3810b1ca18197
dineshbabucittabas <dinesh.b@cittabase.com>
2024-04-24 22:37:18 +0530

    updated always script

--- migrations/A__tracking.sql
+++ migrations/A__tracking.sql
@@ -1,2 +1,3 @@
+create or replace table if not exsists release_entry   select test_seq.nextval as seq , current_timestamp() as Release_time;
 insert into release_entry
  select test_seq.nextval as seq , current_timestamp() as Release_time;
